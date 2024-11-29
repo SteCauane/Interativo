34e612395403079e28fed23df54cd732c3e19bc1
SteCauane <00001115451364SP@al.educacao.sp.gov.br>
2024-11-29 11:19:47 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'BALÉ',
+    'O que é Pirueta?',
+    'A pirueta é um movimento do balé classico onde os bailarinos executam giros. São vários os tipos de piruetas como a en dehors, en dedans, attitude entre outros. '
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'BALÉ',
+    'Por que o plié é o passo mais importante no balé classíco?',
+    'O plé exige do bailarino a execução de movimentos essenciais para executar outros passos como giros e saltos.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'BALÉ',
+    'O que é o fitting?',
+    'O fitting é uma avaliação para escolher a sapatilha de ponta ideal para o bailarino. O fitting dever ser feita por um profissional que pode ser presencial ou online.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'BALÉ',
+    'Por que o fitting é tão importante?',
+    'O fitting serve para escolher a sapatilha de ponta ideal para o bailarino assim evitando futuras lesões.'
 )
